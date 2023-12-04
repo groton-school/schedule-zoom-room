ed1e7baf6f28305aef5a17b05a6833ed41878fc6
Seth Battis <sbattis@groton.org>
2023-12-04 07:11:14 -0500

    more dev

--- bin/setup.mjs
+++ bin/setup.mjs
@@ -1,92 +1,84 @@
 #!/usr/bin/env node
-import gcloud from "@battis/partly-gcloudy";
-import cli from "@battis/qui-cli";
-import fs from "fs";
-import options from "./options.mjs";
+import gcloud from '@battis/partly-gcloudy';
+import cli from '@battis/qui-cli';
+import fs from 'fs';
+import options from './options.mjs';
 
 async function guideBlackbaudAppCreation({
   url,
   accessKey = undefined,
   clientId = undefined,
-  clientSecret = undefined,
+  clientSecret = undefined
 }) {
   accessKey = await cli.prompts.input({
     message: `${options.accessKey.description} from ${cli.colors.url(
       options.accessKey.url
     )}`,
     validate: cli.validators.notEmpty,
-    default: accessKey,
+    default: accessKey
   });
   await cli.prompts.confirm({
-    message: `Create a new app at ${cli.colors.url(options.clientId.url)}`,
+    message: `Create a new app at ${cli.colors.url(options.clientId.url)}`
   });
   clientId = await cli.prompts.input({
     message: options.clientId.description,
     validate: cli.validators.notEmpty,
-    default: clientId,
+    default: clientId
   });
   clientSecret = await cli.prompts.input({
     message: options.clientSecret.description,
     validate: cli.validators.notEmpty,
-    default: clientSecret,
+    default: clientSecret
   });
   const redirectUrl = `${url}/redirect`;
   await cli.prompts.confirm({
     message: `Configure ${cli.colors.value(
       redirectUrl
-    )} as the app's redirect URL`,
+    )} as the app's redirect URL`
   });
   await cli.prompts.confirm({
     message: `Limit the SKY API scopes as described at ${cli.colors.url(
-      "https://github.com/groton-school/blackbaud-to-google-group-sync/blob/main/docs/blackbaud-api-scope.md"
-    )}`,
+      'https://github.com/groton-school/blackbaud-to-google-group-sync/blob/main/docs/blackbaud-api-scope.md'
+    )}`
   });
   return { accessKey, clientId, clientSecret, redirectUrl };
 }
 
 (async () => {
-  const args = gcloud.init({
+  const args = await gcloud.init({
     args: {
       options,
       flags: {
         deploy: {
           description:
-            "Include the (time-consuming) deploy step to App Engine (default true, --no-deploy to skip)",
-        },
-      },
-    },
+            'Include the (time-consuming) deploy step to App Engine (default true, --no-deploy to skip)'
+        }
+      }
+    }
   });
   if (args.values.verbose) {
     cli.shell.setSilent(false);
   }
 
   if (gcloud.ready()) {
-    const { project, appEngine } = await gcloud.batch.appEnginePublish({
+    const { project } = await gcloud.batch.appEnginePublish({
       name: args.values.name,
       id: args.values.project,
-      suggestedName: "Schedule Zoom Room",
+      suggestedName: 'Schedule Zoom Room',
       billingAccountId: args.values.billing,
       region: args.values.region,
-      env: { keys: { urlVar: "URL" } },
+      env: { keys: { urlVar: 'URL' } },
       prebuild: () => {
         return true;
       },
-      deploy: args.values.deploy,
+      deploy: args.values.deploy
     });
-
-    // must create an instance so that IAP can be configured
-    if (!args.values.built) {
-      cli.shell.exec("npm run build");
-    }
-    if (!args.values.deployed) {
-      cli.shell.exec("npm run deploy");
-    }
 
     // enable IAP to limit access to app
     await gcloud.iap.enable({
       applicationTitle: project.name,
       supportEmail: args.values.supportEmail,
-      users: args.values.users,
+      users: args.values.users
     });
 
     // guide storage of Blackbaud credentials in Secret Manager, SKY App creation and configuration
@@ -94,24 +86,24 @@ async function guideBlackbaudAppCreation({
       url,
       accessKey: args.values.accessKey,
       clientId: args.values.clientId,
-      clientSecret: args.values.clientSecret,
+      clientSecret: args.values.clientSecret
     });
     await gcloud.secrets.set({
-      name: "Blackbaud_Subscription_Access_Key",
-      value: blackbaud.accessKey,
+      name: 'Blackbaud_Subscription_Access_Key',
+      value: blackbaud.accessKey
     });
-    await gcloud.secrets.set({ name: "BLACKBAUD_API_TOKEN", value: "null" });
+    await gcloud.secrets.set({ name: 'BLACKBAUD_API_TOKEN', value: 'null' });
     await gcloud.secrets.set({
-      name: "Blackbaud_OAuth2_Client_ID",
-      value: blackbaud.clientId,
-    });
-    await gcloud.secrets.set({
-      name: "Blackbaud_OAuth2_Client_Secret",
-      value: blackbaud.clientSecret,
+      name: 'Blackbaud_OAuth2_Client_ID',
+      value: blackbaud.clientId
     });
     await gcloud.secrets.set({
-      name: "Blackbaud_OAuth2_Redirect_URI",
-      value: blackbaud.redirectUrl,
+      name: 'Blackbaud_OAuth2_Client_Secret',
+      value: blackbaud.clientSecret
+    });
+    await gcloud.secrets.set({
+      name: 'Blackbaud_OAuth2_Redirect_URI',
+      value: blackbaud.redirectUrl
     });
     fs.unlinkSync(credentialsPath);
     await gcloud.secrets.enableAppEngineAccess();
@@ -120,7 +112,7 @@ async function guideBlackbaudAppCreation({
     await cli.prompts.confirm({
       message: `Confirm that you have authorized the app at ${cli.colors.url(
         url
-      )}`,
+      )}`
     });
   }
 })();
